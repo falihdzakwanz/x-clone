6c712787c643af5485d5c736cf95453bac8c5cc4
Falih <falihdzakwan028@gmail.com>
2025-05-05 16:19:22 +0700

    feat: sidebar

---
+++ src/components/Sidebar/SidebarRow/index.tsx
@@ -0,0 +1,17 @@
+import React, { SVGProps, ComponentType } from "react";
+
+interface Props {
+  Icon: ComponentType<SVGProps<SVGSVGElement>>;
+  title: string;
+}
+
+function SidebarRow({ Icon, title }: Props) {
+  return (
+    <div className="flex max-w-fit items-center space-x-2 px-4 py-3 hover:bg-gray-100 rounded-full cursor-pointer transition-all group">
+      <Icon className="h-6 w-6" />
+      <p className="group-hover:text-twitter">{title}</p>
+    </div>
+  );
+}
+
+export default SidebarRow;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CircleStackIcon,
+  EnvelopeIcon,
+  UserIcon,
+  HomeIcon,
+  EllipsisHorizontalCircleIcon
+} from "@heroicons/react/24/outline";
+import Image from "next/image";
+import SidebarRow from "./SidebarRow";
+
+function Sidebar() {
+  return (
+    <div>
+      <Image
+        className="h-10 w-10"
+        src="https://links.papareact.com/drg"
+        alt=""
+        width={10}
+        height={10}
+      />
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={EnvelopeIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CircleStackIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More" />
+    </div>
+  );
+}
+
+export default Sidebar;
